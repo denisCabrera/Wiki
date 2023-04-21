79d594ec0c8f31eb98668e57c5468cae3dc2c3e7
Denis Cabrera <87636792+denisCabrera@users.noreply.github.com>
2023-04-21 08:12:27 +0000

    jekyll initial build

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "00 Purpose and Outline",
+        "excerpt":" I decided to start this wiki/blog to store and share my learning journey through computer science. technology, and cybersecurity.       ","categories": [],
+        "tags": [],
+        "url": "/2023/04/20/FirstPost.html",
+        "teaser": null
+      }]
